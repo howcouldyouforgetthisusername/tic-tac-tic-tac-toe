e36dd649171344fea02c8f843738ad4c1283fe0a
Matthew Morgan <matt@morgan.team>
2017-06-12 15:01:02 -0700

    Add skeleton of main loop

--- base_game.py
+++ base_game.py
@@ -1,1 +1,16 @@
+"""
+This file intended to become a two-human-player, ASCII art game of
+tic-tac-tic-tac-toe.
+"""
 
+
+if __name__ == "__main__":
+	while not done:
+		move = input("Enter your move: ")
+		if is_valid_move(move):
+			board.update(move)
+		else:
+			print("Invalid move!")
+			continue
+		print(board)
+	print(board.endMessage)
